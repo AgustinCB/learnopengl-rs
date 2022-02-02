36b1db04da1acdde3ec5182e722d3169a6b89cbb
Agustin Chiappe Berrini <agustin@chiappeberrini.me>
2022-02-02 06:13:27 -0800

    ANd finish the exercises for the chapter on textures!

---
+++ src/bin/17doubletextureuniform.rs
@@ -0,0 +1,129 @@
+use std::ptr;
+use gl;
+use learnopengl::buffer::Buffer;
+use learnopengl::gl_function;
+use learnopengl::program::Program;
+use learnopengl::shader::Shader;
+use learnopengl::vertex_array::VertexArray;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::video::GLProfile;
+use learnopengl::texture::{Texture, TextureType};
+
+const VERTEX_SHADER: &'static str = include_str!("shaders/03.1-texturevertex.glsl");
+const FRAGMENT_SHADER: &'static str = include_str!("shaders/03.3.4-twotextures.glsl");
+const VERTICES: [f32; 32] = [
+    0.5f32,  0.5, 0.0,   1.0, 0.0, 0.0,   1.0, 1.0,
+    0.5, -0.5, 0.0,   0.0, 1.0, 0.0,   1.0, 0.0,
+    -0.5, -0.5, 0.0,   0.0, 0.0, 1.0,   0.0, 0.0,
+    -0.5,  0.5, 0.0,   1.0, 1.0, 0.0,   0.0, 1.0,
+];
+const INDICES: [u32; 6] = [
+    0, 1, 3, 1, 2, 3,
+];
+
+pub fn main() -> Result<(), String> {
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
+    let attrs = video_subsystem.gl_attr();
+
+    attrs.set_context_major_version(3);
+    attrs.set_context_minor_version(3);
+    attrs.set_context_profile(GLProfile::Core);
+    #[cfg(target_os = "macos")]
+    attrs.set_context_flags().forward_compatible().set();
+
+    let window = video_subsystem
+        .window("rust-sdl2 demo: Video", 800, 600)
+        .position_centered()
+        .opengl()
+        .build()
+        .map_err(|e| e.to_string())?;
+    let _opengl = window.gl_create_context().unwrap();
+    gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
+    let mut event_pump = sdl_context.event_pump()?;
+
+    let program = Program::new(vec![
+        Shader::new(gl::VERTEX_SHADER, VERTEX_SHADER).unwrap(),
+        Shader::new(gl::FRAGMENT_SHADER, FRAGMENT_SHADER).unwrap(),
+    ])?;
+    let vertex_array = VertexArray::new();
+    let array_buffer = Buffer::new(gl::ARRAY_BUFFER);
+    let element_buffer = Buffer::new(gl::ELEMENT_ARRAY_BUFFER);
+    vertex_array.bind();
+    array_buffer.bind();
+    array_buffer.set_data(&VERTICES, gl::STATIC_DRAW);
+    element_buffer.bind();
+    element_buffer.set_data(&INDICES, gl::STATIC_DRAW);
+    VertexArray::set_vertex_attrib_with_padding::<f32>(gl::FLOAT, 0, 8, 3, 0, false);
+    VertexArray::set_vertex_attrib_with_padding::<f32>(gl::FLOAT, 1, 8, 3, 3, false);
+    VertexArray::set_vertex_attrib_with_padding::<f32>(gl::FLOAT, 2, 8, 2, 6, false);
+
+    let texture = Texture::new(TextureType::Texture2D);
+    let texture2 = Texture::new(TextureType::Texture2D);
+    texture.bind(gl::TEXTURE0);
+    texture.set_parameter(gl::TEXTURE_WRAP_S, gl::REPEAT);
+    texture.set_parameter(gl::TEXTURE_WRAP_T, gl::REPEAT);
+    texture.set_parameter(gl::TEXTURE_MIN_FILTER, gl::LINEAR_MIPMAP_LINEAR);
+    texture.set_parameter(gl::TEXTURE_MAG_FILTER, gl::LINEAR);
+    {
+        let data = include_bytes!("../../resource/container.raw");
+        texture.set_image_2d(512, 512, data);
+        texture.generate_mipmap();
+    }
+    texture2.bind(gl::TEXTURE0);
+    texture2.set_parameter(gl::TEXTURE_WRAP_S, gl::REPEAT);
+    texture2.set_parameter(gl::TEXTURE_WRAP_T, gl::REPEAT);
+    texture2.set_parameter(gl::TEXTURE_MIN_FILTER, gl::LINEAR_MIPMAP_LINEAR);
+    texture2.set_parameter(gl::TEXTURE_MAG_FILTER, gl::LINEAR);
+    {
+        let data = include_bytes!("../../resource/face1.raw");
+        texture2.set_image_2d(476, 476, data);
+        texture2.generate_mipmap();
+    }
+    program.use_program();
+    program.set_uniform_i1("texture1", 0);
+    program.set_uniform_i1("texture2", 1);
+    let mut current_blend = 0.2;
+    program.set_uniform_f1("blend", current_blend);
+
+    gl_function!(ClearColor(0.3, 0.3, 0.5, 1.0));
+    'gameloop: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'gameloop,
+                Event::KeyDown {
+                    keycode: Some(Keycode::Right),
+                    ..
+                } => {
+                    current_blend = (current_blend + 0.01).clamp(0.0, 1.0);
+                    program.set_uniform_f1("blend", current_blend);
+                }
+                Event::KeyDown {
+                    keycode: Some(Keycode::Left),
+                    ..
+                } => {
+                    current_blend = (current_blend - 0.01).clamp(0.0, 1.0);
+                    program.set_uniform_f1("blend", current_blend);
+                }
+                _ => {}
+            }
+        }
+
+        gl_function!(Clear(gl::COLOR_BUFFER_BIT));
+        program.use_program();
+        texture.bind(gl::TEXTURE0);
+        texture2.bind(gl::TEXTURE1);
+        vertex_array.bind();
+        gl_function!(DrawElements(gl::TRIANGLES, 6, gl::UNSIGNED_INT, ptr::null()));
+
+        window.gl_swap_window();
+        sdl_context.timer().unwrap().delay(100);
+    }
+
+    Ok(())
+}
