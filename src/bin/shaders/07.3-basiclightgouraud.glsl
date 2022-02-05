03a122846b40b7e36f179788191828fdc9c1db95
Agustin Chiappe Berrini <agustin@chiappeberrini.me>
2022-02-04 20:39:35 -0800

    Exercises chapter 13

---
+++ src/bin/shaders/07.3-basiclightgouraud.glsl
@@ -0,0 +1,34 @@
+#version 330 core
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aNormal;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+uniform vec3 lightColor;
+uniform vec3 lightPos;
+uniform vec3 objectColor;
+uniform vec3 viewPos;
+
+out vec3 Color;
+
+void main() {
+    gl_Position = projection * view * model * vec4(aPos, 1.0);
+    vec3 fragPos = vec3(model * vec4(aPos, 1.0));
+
+    float ambientStrength = 0.1;
+    vec3 ambient = ambientStrength * lightColor;
+
+    vec3 norm = normalize(aNormal);
+    vec3 lightDir = normalize(lightPos - fragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diff * lightColor;
+
+    float specularStrength = 0.5;
+    vec3 viewDir = normalize(viewPos - fragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+    vec3 specular = specularStrength * spec * lightColor;
+
+    Color = (ambient + diffuse + specular) * objectColor;
+}
